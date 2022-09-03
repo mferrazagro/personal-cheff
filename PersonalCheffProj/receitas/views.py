eb4acc0a01557abf93d5b4d718dfee63d7ad4c44
Mauricio Ferraz <104685537+mferrazagro@users.noreply.github.com>
2022-09-03 13:56:41 -0300

    atualização

--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Seja bem vindo</h1>")
+    return render (request,'index.html')
+
+def sucodelaranja(request):
+    return render (request, 'sucodelaranja.html')
+
